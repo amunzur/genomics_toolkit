ad55fb7977115c29775568120b664f92c248f76f
amunzur <admunzur.21@gmail.com>
2025-10-14 13:46:55 -0700

    Added the option to specify partition and time limit in conda. Also now detects path to conda automatically.

--- variant_calling/SLURM_run_vardict.py
+++ variant_calling/SLURM_run_vardict.py
@@ -19,7 +19,7 @@ import argparse
 
 def main():
     # Argument parsing
-    parser = argparse.ArgumentParser(description="Run AA pipeline.")
+    parser = argparse.ArgumentParser(description="Run VarDictJava on a single sample.")
     parser.add_argument("--path_hg38", required=True, help="Path to the reference fasta.")
     parser.add_argument("--threshold_min_vaf", required=True, help="Minimum VAF required to detect mutations.")
     parser.add_argument("--min_alt_reads", required=True, help="Minimum number of alt reads needed to detect a mutation.")
@@ -28,7 +28,10 @@ def main():
     parser.add_argument("--dir_output", required=True, help="Outputted VCF will be saved here.")
     parser.add_argument("--dir_batch_scripts", required=True, help="Batch script will be written here.")
     parser.add_argument("--dir_logs", required=True, help="Vardict logs will be here.")
-
+    parser.add_argument("--dir_vardictjava", required=False, default="", help="Path to the directory containing VardictJava. Defaults to home directory.")
+    parser.add_argument("--sbatch_time_string", required=False, default="29:00", help="Timelimit for sbatch file. Will default to 30 mins.")
+    parser.add_argument("--sbatch_partition", required=False, default="long", help="Partition. Choose from: long, big-mem, normal, express, debug. Defaults to long.")
+    
     args = parser.parse_args()
     
     if "--help" in sys.argv:
@@ -44,6 +47,10 @@ def main():
     path_output=os.path.join(args.dir_output, sample_name+".vcf")
     dir_batch_scripts=args.dir_batch_scripts
     dir_logs=args.dir_logs
+    dir_vardictjava=args.dir_vardictjava
+    sbatch_time_string=args.sbatch_time_string
+    sbatch_partition=args.sbatch_partition
+    
     #
     path_sbatch=os.path.join(dir_batch_scripts, sample_name+".batch")
     path_logs=os.path.join(dir_logs, sample_name+".log")
@@ -55,9 +62,10 @@ def main():
         file.write(f'#SBATCH --job-name={jobname}\n')
         file.write('#SBATCH --cpus-per-task=4\n')
         file.write('#SBATCH --mem=4G\n')
-        file.write('#SBATCH --time=30:00\n')
+        file.write(f'#SBATCH --time={sbatch_time_string}\n')
         file.write(f'#SBATCH --error {path_logs}\n')
         file.write(f'#SBATCH --output {path_logs}\n')
+        file.write(f'#SBATCH --partition={sbatch_partition}\n')
         file.write('\n')
         file.write('# Run Vardict\n')
         file.write(f'/home/jbacon/mambaforge/envs/pipeline/bin/vardict-java \
@@ -67,8 +75,8 @@ def main():
                     -r {min_alt_reads} \
                     -b {path_bam} \
                     -k 0 -c 1 -S 2 -E 3 -g 4 {path_bed} | \
-                    /home/amunzur/VarDictJava/build/install/VarDict/bin/teststrandbias.R | \
-                    /home/amunzur/VarDictJava/build/install/VarDict/bin/var2vcf_valid.pl > {path_output}')
+                    {dir_vardictjava}/VarDictJava/build/install/VarDict/bin/teststrandbias.R | \
+                    {dir_vardictjava}/VarDictJava/build/install/VarDict/bin/var2vcf_valid.pl > {path_output}')
         print(f"sbatch {path_sbatch}")
 
 if __name__ == "__main__":
